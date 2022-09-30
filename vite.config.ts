c70fd93da576414fbdf78cf28f8f5fe677f1a82d
Artem Tarasenko <artem.t@macroactive.com>
2022-09-30 18:03:49 -0400

    Add docker

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
     plugins: [react(), eslint()],
 });
